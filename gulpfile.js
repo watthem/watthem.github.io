6330ff9b0862bffade1b9d11fcd1bde02768c296
Matthew Hendricks <mscohendricks@gmail.com>
2016-12-07 23:31:58 -0600

    changes to style structure to accomodate gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,32 @@
-var gulp = require('gulp');
-var concatCss = require('gulp-concat-css');
+const gulp = require('gulp');
+const changed = require('gulp-changed');
+const concatCss = require('gulp-concat-css');
+const watch = require('gulp-watch');
+const autoprefixer = require('gulp-autoprefixer');
 
+const SOURCE = './src/dev/style/*.css';
+const DESTINATION = './src/pub/style/';
+const APP_STYLESHEET = 'app.css';
 
-gulp.task('concat', function() {
-    return gulp.src('./src/style/*.css')
-        .pipe(concatCss("app.css"))
-        .pipe(gulp.dest('./src/style/'));
+const PUBFILE = DESTINATION + APP_STYLESHEET;
+
+gulp.task('css', () => {
+    return gulp.src(SOURCE)
+        .pipe(autoprefixer({
+            browsers: ['last 2 version', 'safari 5', 'ie 6', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
+        }))
+        .pipe(concatCss(APP_STYLESHEET))
+        .pipe(gulp.dest(DESTINATION));
 });
 
-gulp.task('default', ['concat']);
+gulp.task('watch', function() {
+    gulp.watch(SOURCE, ['css']);
+
+})
+
+
+gulp.task('default', ['css'], () => {
+    return gulp.src(SOURCE)
+        .pipe(changed(DESTINATION))
+        .pipe(gulp.dest(DESTINATION));
+});
